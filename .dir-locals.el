c40bbf2bc21546cf1dab081c6156ea9b95e2e93e
Johannes Lundberg <johalun0@gmail.com>
2020-04-15 15:29:47 -0700

    Add Emacs .dir-locals.el file.

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;; rename to .dir-locals.el
+((c-mode . ((c-file-style . "linux-tabs-only")
+            (indent-tabs-mode . t)
+            (show-trailing-whitespace . t)
+            (c-basic-offset . 8)
+            (tab-width . 8)
+            ))
+ )
